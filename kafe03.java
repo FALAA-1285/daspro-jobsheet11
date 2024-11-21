d6938bd7f42631437eec2e75e17de42cf5862b6b
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-11-21 08:27:40 +0700

    percobaan2

--- kafe03.java
+++ kafe03.java
@@ -1,5 +1,10 @@
 public class kafe03{
-    public static void main(String[] args) {
+    public static void menu(String namaPelanggan, boolean isMember){
+        System.out.println("Selamat datang, "+namaPelanggan+"!");
+
+        if(isMember){
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap bembelian");
+        }
         System.out.println("=====MENU RESTO KAFE=====");
         System.out.println("1. kopi hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -9,5 +14,8 @@ public class kafe03{
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("==========================");
         System.out.println("silahkan pilih menu yang anda inginkan");
+    }
+    public static void main(String[] args) {
+        menu("Andi", true);
     }
 }
